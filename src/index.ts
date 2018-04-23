cd00580046e8df06f3bc2c54ad5e20cc92bb2818
Matias Alvin <alvinmatias69@gmail.com>
2018-04-23 19:34:55 +0700

    cors added

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
+import * as cors from 'cors';
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ const app: express.Application = express();
 const port: number = Number(process.env.PORT) || 3000;
 const host: string = process.env.HOST || 'localhost';
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
